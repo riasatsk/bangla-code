7e27da85fab6a60d0378824d7fe9dc33282868eb
Riasat <riasatofficial@outlook.com>
2023-11-24 09:16:07 +0530

    modified:   src/bangla/bangla.ts

--- src/bangla/bangla.ts
+++ src/bangla/bangla.ts
@@ -1,7 +1,7 @@
 import { convertToJS } from "./replacer.js";
 import { std } from "./std.js";
 
-export function execute(code) {
+export function execute(code: string) {
   const paresedCode = convertToJS(code);
   const main = new Function(`${paresedCode}`);
   const mainWithSTD = main.bind(std);
